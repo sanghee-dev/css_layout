41af3883712e6b9b6c55b43bdd4d3bb6836375c6
leeedev <leeesangheee@gmail.com>
2020-12-13 03:23:11 +0900

    Create css layout page

---
+++ index.js
@@ -0,0 +1,23 @@
+const articles = Array.from(document.querySelectorAll("article"));
+const vw = window.innerWidth;
+
+articles.forEach((article) => {
+  const background = article.style.backgroundImage;
+  const image = new Image();
+  image.src = background.replace(/url\((['"])?(.*?)\1\)/gi, "$2");
+  const width = image.width;
+  const height = image.height;
+  console.log(width, height);
+  if (width < height) {
+    const h = height - 0.3 * vw;
+    article.style.backgroundSize = "100% auto";
+    article.style.animation = `scrollY ${h / 100}s linear infinite`;
+  } else {
+    article.style.backgroundSize = "auto 100%";
+    article.style.animation = `scrollX ${30000 / height}s linear infinite`;
+  }
+});
+
+// const test = "url(images/foreword-contributors.jpg)";
+// const answer = test.replace(/url\((['"])?(.*?)\1\)/gi, "$2");
+// console.log(answer);
